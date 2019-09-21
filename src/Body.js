5b89ff8db71809b599a5a42050c6b713522ae83b
marianoiglesiasmarchese <marianoiglesiasmarchese@gmail.com>
2019-09-21 09:41:45 -0300

    feature(2): create page template

---
+++ src/Body.js
@@ -0,0 +1,16 @@
+import React from 'react';
+import PaginatedSwiper from './components/swiper/PaginatedSwiper';
+
+import "react-id-swiper/lib/styles/css/swiper.css";
+
+class Body extends React.Component{
+    render() {
+        return (
+            
+            <PaginatedSwiper/>  
+            
+        )
+    }
+}
+
+export default Body;
---
+++ src/Header.js
@@ -0,0 +1,15 @@
+import React from 'react';
+
+class Header extends React.Component{
+    render() {
+        return (
+            <header className="App-header">
+            <p>
+              Edit <code>src/App.js</code> and save to reload.
+            </p>
+          </header>
+        )
+    }
+}
+
+export default Header;

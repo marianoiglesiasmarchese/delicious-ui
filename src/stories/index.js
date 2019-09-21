d75b8f7421c7b743b08a2af4b4143f7275dd5429
marianoiglesiasmarchese <marianoiglesiasmarchese@gmail.com>
2019-09-21 09:40:01 -0300

    feature(14): add storybook

---
+++ src/stories/index.js
@@ -0,0 +1,27 @@
+import React from 'react';
+
+import { storiesOf } from '@storybook/react';
+import { action } from '@storybook/addon-actions';
+import { linkTo } from '@storybook/addon-links';
+
+import { Button, Welcome } from '@storybook/react/demo';
+import Default from '../components/swiper/SimpleSwiper';
+// import Swiper from 'react-id-swiper';
+
+import "react-id-swiper/lib/styles/css/swiper.css";
+
+storiesOf('Welcome', module).add('to Storybook', () => <Welcome showApp={linkTo('Button')} />);
+
+storiesOf('Button', module)
+  .add('with text', () => <Button onClick={action('clicked')}>Hello Button</Button>)
+  .add('with some emoji', () => (
+    <Button onClick={action('clicked')}>
+      <span role="img" aria-label="so cool">
+        😀 😎 👍 💯
+      </span>
+    </Button>
+  ));
+
+storiesOf('Body', module).add('swiper', () => (  
+<Default/>
+));
